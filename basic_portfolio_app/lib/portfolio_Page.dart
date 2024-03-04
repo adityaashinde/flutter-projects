31f3b84a6954a2e6918ecbad1ab386d7569e7eca
adityaashinde <adityashinde4580@gmail.com>
2024-03-04 12:57:54 +0530

    Portfolio-App

--- basic_portfolio_app/lib/portfolio_Page.dart
+++ basic_portfolio_app/lib/portfolio_Page.dart
@@ -18,16 +18,146 @@ class _PortfolioState extends State<Portfolio> {
     });
   }
 
+  Widget one() {
+    return const Text(
+      "Name : Aditya Ashok Shinde",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget two() {
+    return Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: Colors.amber,
+        ),
+      ),
+      child: Image.asset(
+        "assets/images/profile.jpg",
+        height: 200,
+        width: 200,
+      ),
+    );
+  }
+
+  Widget three() {
+    return const Text(
+      "College Name : Smt. Kashibai Navle College of Engineering Vadgoan Bk, Pune",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget four() {
+    return Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: Colors.amber,
+        ),
+      ),
+      child: Image.asset(
+        "assets/images/STES.png",
+        height: 200,
+        width: 200,
+      ),
+    );
+  }
+
+  Widget five() {
+    return const Text(
+      "Dream Company : Adobe",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget six() {
+    return Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: Colors.amber,
+        ),
+      ),
+      child: Image.asset(
+        "assets/images/adobe.png",
+        height: 200,
+        width: 200,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Portfolio App",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
         ),
+        centerTitle: true,
         backgroundColor: Colors.amber,
       ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.amber.shade100,
+        padding: const EdgeInsets.only(top: 40, left: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            (count >= 0) ? one() : const Text(""),
+            const SizedBox(
+              height: 10,
+            ),
+            (count >= 1) ? two() : Container(),
+            const SizedBox(
+              height: 10,
+            ),
+            (count >= 2) ? three() : const Text(""),
+            const SizedBox(
+              height: 10,
+            ),
+            (count >= 3) ? four() : Container(),
+            const SizedBox(
+              height: 10,
+            ),
+            (count >= 4) ? five() : const Text(""),
+            const SizedBox(
+              height: 10,
+            ),
+            (count >= 5) ? six() : Container(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          hoverColor: Colors.amberAccent,
+          onPressed: counter,
+          child: const Icon(
+            Icons.arrow_right,
+          )),
     );
   }
 }
